67c7e4a949de81c97610f2df8ec232fb52e0feb7
Step <62307731+KarapuzDev@users.noreply.github.com>
2024-05-20 00:28:28 +0300

    Настроенный докер Sql + Rails

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    calidates :name, presence: true, lenght: { maximum: 50 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                      format: { with: VALID_EMAIL_REGEX },
+                      uniqueness: { case_sensitive: false }
+end
