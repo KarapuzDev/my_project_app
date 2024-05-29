91dbea099447ad20c07c992e61449dfe79e62838
KarapuzDev <ssttepp2@mail.ru>
2024-05-29 15:53:48 +0300

    Закончил с авторизацией

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,18 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
+
   validates :name, presence: true, length: { maximum: 50 }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
+
   has_secure_password
+
   validates :password, length: { minimum: 6}
 
   #Возвращает дайджест для указаной строки
@@ -32,6 +38,12 @@ class User < ApplicationRecord
 
   #Возвращает true, если указанный токен соответствует дайджесту.
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest.is_password?(remember_token))
+  end
+
+  # Забывает пользователя
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
